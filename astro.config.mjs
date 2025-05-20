14fc04c8113ccac2bdf7700ed52f82197e4b7ead
Tim <github@timgreen.ws>
2025-05-20 18:17:10 +1000

    added astro-minify-html-swc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import minify from 'astro-minify-html-swc'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), minify()],
   outDir: './harlow',
   build: {
     assetsPrefix: '/harlow'
